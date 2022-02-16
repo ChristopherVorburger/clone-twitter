ad42e639f47e8684dd53a105e58c2ff028087a36
NaLuxis <yoannalves7@gmail.com>
2022-02-16 19:10:14 +0100

    Revert "recup package-lock.json"
    This reverts commit e631949a9592b2d87c8231155800642e5ab65b0d.

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,15 +1,5 @@
 import React from "react";
-import {
-  Overlay,
-  LoginModalContainer,
-  LoginModal,
-  IconTwitter,
-  LoginTitle,
-  LoginContent,
-  ButtonLogin,
-  Line,
-  TxtCreateAcount,
-} from "./Login.Style";
+import { Overlay, LoginModalContainer, LoginModal, IconTwitter, LoginTitle, LoginContent, ButtonLogin, Line, TxtCreateAcount } from "./Login.Style";
 import LogoTwitter from "./img/logo-twitter.png";
 import LogoGoogle from "./img/logo-google.png";
 import LogoApple from "./img/logo-apple.png";
@@ -36,36 +26,24 @@ export default function Login() {
           </div>
           <LoginTitle>Connectez-vous à Twitter</LoginTitle>
 
-          <ButtonLogin borderColor="#dadce0" bg="transparent" mb="25px">
-            <img src={LogoGoogle} alt="logo de google" />
+          <ButtonLogin borderColor='#dadce0' bg='transparent' mb='25px'>
+            <img src={LogoGoogle} alt='logo de google' />
             <span>Se connecter avec Google</span>
           </ButtonLogin>
-          <ButtonLogin bg="transparent" borderColor="#dadce0">
+          <ButtonLogin bg='transparent' borderColor='#dadce0'>
             <img src={LogoApple} alt="logo d'apple" />
             <span>Se connecter avec Apple</span>
           </ButtonLogin>
           <Line>ou</Line>
-          <TextField
-            size="medium"
-            id="outlined-basic"
-            label="Adresse mail"
-            variant="outlined"
-            className={classes.input}
-          />
-          <ButtonLogin
-            bg="black"
-            color="white"
-            borderColor="black"
-            bold
-            mb="25px"
-          >
+          <TextField size='medium' id='outlined-basic' label='Adresse mail' variant='outlined' className={classes.input} />
+          <ButtonLogin bg='black' color='white' borderColor='black' bold mb='25px'>
             <span>Suivant</span>
           </ButtonLogin>
-          <ButtonLogin bg="#fff" color="#000" borderColor="#dadce0">
+          <ButtonLogin bg='#fff' color='#000' borderColor='#dadce0'>
             <span>Mot de passe oublié ?</span>
           </ButtonLogin>
           <TxtCreateAcount>
-            Vous n'avez pas de compte ?<a href="/">Inscrivez-vous</a>
+            Vous n'avez pas de compte ?<a href='/'>Inscrivez-vous</a>
           </TxtCreateAcount>
         </LoginModal>
       </LoginModalContainer>
