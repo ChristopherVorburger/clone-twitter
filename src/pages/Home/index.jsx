eddffec3ec003984447123c5add2d950307bf632
Chris <christopher.vorburger2@gmail.com>
2022-02-17 17:45:13 -0600

    add button new tweet

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,9 @@
-import { Button, Container, Typography } from "@mui/material";
+import { Box, Button, Container, Typography } from "@mui/material";
 import React from "react";
 import Header from "../../components/Header";
 import BottomNavigation from "../../components/BottomNavigation";
+
+import { GiFeather } from "react-icons/gi";
 
 import useStyles from "./styles";
 
@@ -12,7 +14,7 @@ const Home = () => {
     <>
       <Header />
       <main>
-        <div>
+        <Box height="100vh">
           <Container sx={{ margin: "30px auto", padding: "0 30px" }}>
             <Typography mb="0.5rem" fontWeight="bold" fontSize="29px">
               Welcome to Twitter!
@@ -34,7 +36,11 @@ const Home = () => {
               <Typography fontWeight="bold">Let's go!</Typography>
             </Button>
           </Container>
-        </div>
+          <Box className={classes.button__feather}>
+            <div className={classes.button__feather_plus}>+</div>
+            <GiFeather size="1.7rem" color="white" />
+          </Box>
+        </Box>
       </main>
       <BottomNavigation />
     </>
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,6 +1,6 @@
 import { makeStyles } from "@mui/styles";
 
-export default makeStyles(() => ({
+export default makeStyles((theme) => ({
   button: {
     borderRadius: "40px!important",
     textTransform: "none!important",
@@ -8,5 +8,19 @@ export default makeStyles(() => ({
     maxWidth: "380px",
     minWidth: "49px!important",
     minHeight: "49px!important",
+  },
+  button__feather: {
+    backgroundColor: theme.palette.primary.main,
+    borderRadius: "50%",
+    position: "absolute",
+    right: "19px",
+    bottom: "80px",
+    padding: "0.8rem",
+  },
+  button__feather_plus: {
+    position: "absolute",
+    right: "35px",
+    bottom: "30px",
+    color: "white",
   },
 }));
