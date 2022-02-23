c64b0d4e23199ec2fd88b4730bfb266072c20da1
NaLuxis <yoannalves7@gmail.com>
2022-02-23 23:28:12 +0100

    ajout auth

--- src/pages/SignUp/SingUp.js
+++ src/pages/SignUp/SingUp.js
@@ -7,38 +7,82 @@ import MenuItem from "@mui/material/MenuItem";
 import { selectMonth } from "./DataSelect";
 import { Link } from "react-router-dom";
 import { db } from "../../firebase-config";
-import { collection, getDocs, addDoc } from "firebase/firestore";
+import {
+  collection,
+  getDocs,
+  addDoc,
+  serverTimestamp,
+} from "firebase/firestore";
+import { AuthContext } from "../../context/authContext";
 
 function SignUp() {
   const classes = useStyles();
+  const auth = React.useContext(AuthContext);
 
   const [newName, setNewName] = React.useState("");
   const [newEmail, setNewEmail] = React.useState("");
+  const [newEmailSignup, setNewEmailSignup] = React.useState("");
   const [newPhone, setNewPhone] = React.useState("");
-  const [newPassword, setNewPassword] = React.useState("");
+  const [newPasswordSignup, setNewPasswordSignup] = React.useState("");
   const [newMonth, setNewMonth] = React.useState("");
   const [newDay, setNewDay] = React.useState(0);
   const [newYear, setNewYear] = React.useState(0);
 
-  const [users, setUsers] = useState([]);
-  const usersCollectionRef = collection(db, "users");
+  // const [users, setUsers] = useState([]);
+  // const usersCollectionRef = collection(db, "users");
 
-  const createUser = async () => {
-    await addDoc(usersCollectionRef, {
-      newName,
-      newEmail,
-      newPhone,
-    });
-  };
+  const signupForm = document.querySelector("#signup-form");
+  console.log(signupForm);
+  signupForm.addEventListener("submit", (e) => {
+    e.preventDefault();
 
-  useEffect(() => {
-    const getUsers = async () => {
-      const data = await getDocs(usersCollectionRef);
-      setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-    };
+    const email = signupForm["signup-email"].value;
+    const password = signupForm["signup-password"].value;
 
-    getUsers();
-  }, []);
+    // sign up the user & add firestore data
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((cred) => {
+        return db.collection("users").doc(cred.user.uid).set({
+          bio: signupForm["signup-bio"].value,
+        });
+      })
+      .then(() => {
+        signupForm.reset();
+      });
+  });
+
+  // const createUser = async () => {
+  //   await addDoc(usersCollectionRef, {
+  //     name: newName,
+  //     email: newEmail,
+  //     phone: newPhone,
+  //     created_at: serverTimestamp(),
+  //   });
+  // };
+  // const auth = React.useContext(AuthContext);
+  // const signUp = (e) => {
+  //   e.preventDefault();
+  //   auth
+  //     .signUp(newEmailSignup, newPasswordSignup)
+  //     .then((cred) => {
+  //       setNewEmailSignup("");
+  //       setNewPasswordSignup("");
+  //       console.log("user created : ", cred.user);
+  //     })
+  //     .catch((err) => {
+  //       console.log(err.message);
+  //     });
+  // };
+
+  // useEffect(() => {
+  //   const getUsers = async () => {
+  //     const data = await getDocs(usersCollectionRef);
+  //     setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+  //   };
+
+  //   getUsers();
+  // }, []);
 
   const [switchPhoneEmail, setSwitchPhoneEmail] = React.useState("Email");
   const [errorName, setErrorName] = React.useState(false);
@@ -89,10 +133,11 @@ function SignUp() {
   function MMDDYYYYInput() {
     return (
       <Stack direction="row" marginTop="15px" spacing={2}>
-        <Box component="form" width="48%">
+        <Box width="48%">
           <TextField
             select
             label="Mois"
+            required
             value={month}
             onChange={(e) => {
               setMonth(e.target.value);
@@ -110,10 +155,11 @@ function SignUp() {
             ))}
           </TextField>
         </Box>
-        <Box component="form" width="22%">
+        <Box width="22%">
           <TextField
             select
             label="Jour"
+            required
             value={day}
             onChange={(e) => {
               setDay(e.target.value);
@@ -131,10 +177,11 @@ function SignUp() {
             ))}
           </TextField>
         </Box>
-        <Box component="form" width="30%">
+        <Box width="30%">
           <TextField
             select
             label="Année"
+            required
             value={year}
             onChange={(e) => {
               setYear(e.target.value);
@@ -173,129 +220,134 @@ function SignUp() {
             <LogoTwitter />
           </Stack>
         </Stack>
-        <Stack
-          className={classes.signupContainer}
-          width="100%"
-          justifyContent="center"
-          spacing={2}
-        >
-          <Stack spacing={4}>
-            <Typography className={classes.accountCreateTitle}>
-              Créer votre compte
-            </Typography>
+        <form id="signup-form">
+          <Stack
+            className={classes.signupContainer}
+            width="100%"
+            justifyContent="center"
+            spacing={2}
+          >
             <Stack spacing={4}>
-              <Box component="form">
-                <TextField
-                  type="text"
-                  variant="outlined"
-                  label="Nom et prénom"
-                  fullWidth={true}
-                  autoFocus={true}
-                  onChange={(e) => {
-                    e.target.value.length >= 2
-                      ? setErrorName(false)
-                      : setErrorName(true);
-                    setNewName(e.target.value);
-                  }}
-                  error={errorName}
-                  helperText={
-                    errorName === true ? "Quel est votre nom ?" : null
-                  }
-                />
-              </Box>
-              {switchPhoneEmail === "Phone" ? (
-                <Box component="form">
+              <Typography className={classes.accountCreateTitle}>
+                Créer votre compte
+              </Typography>
+              <Stack spacing={4}>
+                <Box>
                   <TextField
                     type="text"
                     variant="outlined"
-                    label="Email"
+                    label="Nom et prénom"
+                    required
                     fullWidth={true}
+                    autoFocus={true}
                     onChange={(e) => {
-                      e.target.value.includes("@") &&
-                      e.target.value.includes(".")
-                        ? setErrorEmail(false)
-                        : setErrorEmail(true);
-                      setNewEmail(e.target.value);
+                      e.target.value.length >= 2
+                        ? setErrorName(false)
+                        : setErrorName(true);
+                      setNewName(e.target.value);
                     }}
-                    error={errorEmail}
+                    error={errorName}
                     helperText={
-                      errorEmail === true
-                        ? "Veuillez entrer un email valide."
-                        : null
+                      errorName === true ? "Quel est votre nom ?" : null
                     }
                   />
                 </Box>
-              ) : switchPhoneEmail === "Email" ? (
-                <Box component="form">
-                  <TextField
-                    type="number"
-                    variant="outlined"
-                    label="Téléphone"
-                    fullWidth={true}
-                    onChange={(e) => {
-                      e.target.value.length >= 10
-                        ? setErrorPhone(false)
-                        : setErrorPhone(true);
-                      setNewPhone(e.target.value);
-                    }}
-                    error={errorPhone}
-                    helperText={
-                      errorPhone === true
-                        ? "Veuillez entrer un numéro de téléphone valide."
-                        : null
-                    }
-                  />
-                </Box>
-              ) : null}
+                {switchPhoneEmail === "Phone" ? (
+                  <Box>
+                    <TextField
+                      id="signup-email"
+                      type="text"
+                      variant="outlined"
+                      label="Email"
+                      required
+                      fullWidth={true}
+                      onChange={(e) => {
+                        e.target.value.includes("@") &&
+                        e.target.value.includes(".")
+                          ? setErrorEmail(false)
+                          : setErrorEmail(true);
+                        setNewEmail(e.target.value);
+                        setNewEmailSignup(e.target.value);
+                      }}
+                      error={errorEmail}
+                      helperText={
+                        errorEmail === true
+                          ? "Veuillez entrer un email valide."
+                          : null
+                      }
+                    />
+                  </Box>
+                ) : switchPhoneEmail === "Email" ? (
+                  <Box>
+                    <TextField
+                      type="number"
+                      variant="outlined"
+                      label="Téléphone"
+                      required
+                      fullWidth={true}
+                      onChange={(e) => {
+                        e.target.value.length >= 10
+                          ? setErrorPhone(false)
+                          : setErrorPhone(true);
+                        setNewPhone(e.target.value);
+                      }}
+                      error={errorPhone}
+                      helperText={
+                        errorPhone === true
+                          ? "Veuillez entrer un numéro de téléphone valide."
+                          : null
+                      }
+                    />
+                  </Box>
+                ) : null}
+              </Stack>
+              <SwitchPhoneEmail />
             </Stack>
-            <SwitchPhoneEmail />
-          </Stack>
-          <Typography className={classes.birthdayPasswordTitle}>
-            Créer votre mot de passe
-          </Typography>
-          <Box component="form">
-            <TextField
-              type="password"
-              variant="outlined"
-              label="Mot de passe"
-              fullWidth={true}
-              onChange={(e) => {
-                e.target.value.length >= 6
-                  ? setPasswordIsOk(false)
-                  : setPasswordIsOk(true);
-                setNewPassword(e.target.value);
-              }}
-              error={passwordIsOk}
-              helperText={
-                passwordIsOk === true
-                  ? "Votre mot de passe est trop cour"
-                  : null
-              }
-            />
-          </Box>
-          <Stack>
             <Typography className={classes.birthdayPasswordTitle}>
-              Date de naissance
+              Créer votre mot de passe
             </Typography>
-            <Typography className={classes.birthdayText}>
-              Cette information ne sera pas affichée publiquement. Confirmez
-              votre âge, même si ce compte est pour une entreprise, un animal de
-              compagnie ou autre chose.
-            </Typography>
-            <MMDDYYYYInput />
+            <Box>
+              <TextField
+                id="signup-password"
+                type="password"
+                variant="outlined"
+                label="Mot de passe"
+                required
+                fullWidth={true}
+                onChange={(e) => {
+                  e.target.value.length >= 6
+                    ? setPasswordIsOk(false)
+                    : setPasswordIsOk(true);
+                  setNewPasswordSignup(e.target.value);
+                }}
+                error={passwordIsOk}
+                helperText={
+                  passwordIsOk === true
+                    ? "Votre mot de passe est trop cour"
+                    : null
+                }
+              />
+            </Box>
+            <Stack>
+              <Typography className={classes.birthdayPasswordTitle}>
+                Date de naissance
+              </Typography>
+              <Typography className={classes.birthdayText}>
+                Cette information ne sera pas affichée publiquement. Confirmez
+                votre âge, même si ce compte est pour une entreprise, un animal
+                de compagnie ou autre chose.
+              </Typography>
+              <MMDDYYYYInput />
+            </Stack>
+            <Stack backgroundColor="white">
+              {/* <Link to={"/home"}> */}
+              <Button type="submit" className={classes.nextButton} size="large">
+                Suivant
+              </Button>
+              {/* </Link> */}
+            </Stack>
           </Stack>
-          <Stack backgroundColor="white">
-            {/* <Link to={"/home"}> */}
-            <Button
-              className={classes.nextButton}
-              size="large"
-              onClick={createUser}
-            >
-              Suivant
-            </Button>
-            {/* </Link> */}
-          </Stack>
-        </Stack>
+        </form>
       </Stack>
     </div>
   );
