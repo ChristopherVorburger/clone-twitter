71b86814bfbb02eedfe788e15407e253be20bc84
Chris <christopher.vorburger2@gmail.com>
2022-02-23 10:40:47 -0600

    exemple firebase

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Prehome from "./pages/Prehome";
 import SignUp from "./pages/SignUp/SingUp";
 import Login from "./components/Login/Login";
 import Home from "./pages/Home";
+import Exemple from "./components/Exemple";
 
 export default function App() {
   // Création d'un thème pour changer la couleur principale de MUI
@@ -35,6 +36,7 @@ export default function App() {
         <Route path="/signup" element={<SignUp />} />
         <Route path="/login" element={<Login />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/exemple" element={<Exemple />} />
       </Routes>
     </ThemeProvider>
   );
