c99b488b6a2bd3cbf91d9969b698bfff17d45b6c
Bourhane FIDAHOUSSEN PATEL <bourhane.fidahoussen@gmail.com>
2022-03-10 20:53:06 +0100

    Ajout route message suite au merge

--- src/App.js
+++ src/App.js
@@ -1,42 +1,43 @@
-import React from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
-import { ThemeProvider, createTheme } from "@mui/material/styles";
-import Prehome from "./pages/Prehome";
-import SignUp from "./pages/SignUp/SingUp";
-import Login from "./components/Login/Login";
-import Home from "./pages/Home";
-import Exemple from "./components/Exemple";
-import Profile from "./pages/Profile";
-import Followers from "./pages/Followers";
-import Following from "./pages/Following";
-import EditProfileModal from "./components/EditProfileModal";
-import Layout from "./components/Layout";
-import Bookmarks from "./pages/Bookmarks";
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import Prehome from './pages/Prehome';
+import SignUp from './pages/SignUp/SingUp';
+import Login from './components/Login/Login';
+import Home from './pages/Home';
+import Exemple from './components/Exemple';
+import Profile from './pages/Profile';
+import Followers from './pages/Followers';
+import Following from './pages/Following';
+import EditProfileModal from './components/EditProfileModal';
+import Layout from './components/Layout';
+import Bookmarks from './pages/Bookmarks';
+import Message from './components/Message/Message';
 
-import { AuthContext } from "./context/authContext";
-import { SnackbarsContextProvider } from "./context/snackbarsContext";
+import { AuthContext } from './context/authContext';
+import { SnackbarsContextProvider } from './context/snackbarsContext';
 
 export default function App() {
   const auth = React.useContext(AuthContext);
-  console.log("auth", auth);
+  console.log('auth', auth);
   // Création d'un thème pour changer la couleur principale de MUI
   let theme = createTheme({
     palette: {
       primary: {
-        main: "#1d9bf0",
+        main: '#1d9bf0',
       },
       black: {
-        main: "#282d31",
-        darker: "#0f1419",
+        main: '#282d31',
+        darker: '#0f1419',
       },
       grey: {
-        main: "#536471",
-        button: "#cfd9de",
-        background__input: "#eff3f4",
-        background__trend: "#f7f9f9",
+        main: '#536471',
+        button: '#cfd9de',
+        background__input: '#eff3f4',
+        background__trend: '#f7f9f9',
       },
       white: {
-        main: "#ffffff",
+        main: '#ffffff',
       },
     },
     typography: {
@@ -71,6 +72,7 @@ export default function App() {
               <Route path="/:username/following" element={<Following />} />
               <Route path="/settings/profile" element={<EditProfileModal />} />
               <Route path="/bookmarks" element={<Bookmarks />} />
+              <Route path="/messages" element={<Message />} />
               <Route path="*" element={<Navigate to="/home" />} />
             </Routes>
           </SnackbarsContextProvider>
