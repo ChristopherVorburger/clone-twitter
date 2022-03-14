07800ac722b0eb2cf7e856b5cb57a450f21401be
Bourhane FIDAHOUSSEN PATEL <bourhane.fidahoussen@gmail.com>
2022-03-14 17:03:08 +0100

    Affichage icon de rwitter pour l'ajout de conversation

--- src/components/Message/Message.jsx
+++ src/components/Message/Message.jsx
@@ -209,7 +209,7 @@ export default function Messages() {
           flexDirection="column"
           borderLeft="1px solid #eff3f4"
           borderRight="1px solid #eff3f4"
-          width="100%"
+          // width="50%"
         >
           {/* Entête */}
           <Box display="flex" flexDirection="row">
@@ -220,13 +220,37 @@ export default function Messages() {
               />
             </Box>
 
-            <Box style={{ padding: '1.2em' }}>
+            <Box style={{ padding: '1.2rem' }}>
               <Button
                 variant="outlined"
                 onClick={() => navigate('/searchUser')}
                 className={classes.profile__button}
+                style={{
+                  border: 'none',
+                }}
               >
-                <Icons.AddCommentIcon />
+                {/* <Icons.AddCommentIcon /> */}
+
+                <svg
+                  viewBox="0 0 24 24"
+                  aria-hidden="true"
+                  class="r-18jsvk2 r-4qtqp9 r-yyyyoo r-z80fyv r-dnmrzs r-bnwqim r-1plcrui r-lrvibr r-19wmn03"
+                >
+                  <g>
+                    <path d="M23.25 3.25h-2.425V.825c0-.414-.336-.75-.75-.75s-.75.336-.75.75V3.25H16.9c-.414 0-.75.336-.75.75s.336.75.75.75h2.425v2.425c0 .414.336.75.75.75s.75-.336.75-.75V4.75h2.425c.414 0 .75-.336.75-.75s-.336-.75-.75-.75zm-3.175 6.876c-.414 0-.75.336-.75.75v8.078c0 .414-.337.75-.75.75H4.095c-.412 0-.75-.336-.75-.75V8.298l6.778 4.518c.368.246.79.37 1.213.37.422 0 .844-.124 1.212-.37l4.53-3.013c.336-.223.428-.676.204-1.012-.223-.332-.675-.425-1.012-.2l-4.53 3.014c-.246.162-.563.163-.808 0l-7.586-5.06V5.5c0-.414.337-.75.75-.75h9.094c.414 0 .75-.336.75-.75s-.336-.75-.75-.75H4.096c-1.24 0-2.25 1.01-2.25 2.25v13.455c0 1.24 1.01 2.25 2.25 2.25h14.48c1.24 0 2.25-1.01 2.25-2.25v-8.078c0-.415-.337-.75-.75-.75z"></path>
+                  </g>
+                </svg>
+
+                {/* <div
+                  dir="auto"
+                  className="css-901oao r-1awozwy r-18jsvk2 r-6koalj r-18u37iz r-16y2uox r-37j5jr r-a023e6 r-b88u0q r-1777fci r-rjixqe r-bcqeeo r-q4m81j r-qvutc0"
+                >
+                  
+                  <span
+                    className="css-901oao css-16my406 css-bfa6kz r-poiln3 r-a023e6 r-rjixqe r-bcqeeo r-qvutc0"
+                    style="border-bottom: 2px solid rgb(15, 20, 25);"
+                  ></span>
+                </div> */}
               </Button>
             </Box>
           </Box>
@@ -237,8 +261,8 @@ export default function Messages() {
             <Divider sx={{ borderColor: 'background__input' }} />
           </Box>
 
+          {/* Affichage de la liste des conversations */}
           <Box>
-            {/* Affichage de la liste des conversations */}
             <ListeChannels
               channels={channels}
               handleDiplayMessages={(idChannels) =>
@@ -254,7 +278,10 @@ export default function Messages() {
         </Box>
 
         {/* Zone à droite */}
-        <Box display="flex" minWidth="350px">
+        <Box
+          display="flex"
+          // width="50%"
+        >
           {/* Afficahge des messages */}
           <Message
             messages={messages}
