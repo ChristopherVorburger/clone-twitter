bf9eeb348f8e8059238e43f80b4f141bf6696969
Bourhane FIDAHOUSSEN PATEL <bourhane.fidahoussen@gmail.com>
2022-03-21 21:19:04 +0100

    Clean code

--- src/components/Message/ChannelAddMessage/ChannelAddMessage.jsx
+++ src/components/Message/ChannelAddMessage/ChannelAddMessage.jsx
@@ -1,30 +1,19 @@
-import React from 'react';
+import React from "react";
+import { Button, Divider, TextField } from "@mui/material";
+import { Box } from "@mui/system";
+import { icons } from "../../../constants";
+import useStyles from "./styles";
 
-import { Button, Divider, TextField, Typography } from '@mui/material';
-import { Box } from '@mui/system';
-
-import { icons, images } from '../../../constants';
-
-import useStyles from './styles';
-
-import { AuthContext } from '../../../context/authContext';
-
-const ChannelAddMessage = ({
-  handleAddNewMessage,
-  // newMessage,
-  // setNewMessage,
-}) => {
-  const auth = React.useContext(AuthContext);
-  const [newMessage, setNewMessage] = React.useState('');
+const ChannelAddMessage = ({ handleAddNewMessage }) => {
+  const [newMessage, setNewMessage] = React.useState("");
 
   const classes = useStyles();
   return (
     <>
-      <Divider sx={{ borderColor: 'background__input' }} />
+      <Divider sx={{ borderColor: "background__input" }} />
       <Box
         sx={{
-          // borderRadius: '50px',
-          paddingBottom: '23px',
+          paddingBottom: "23px",
         }}
       >
         <Box
@@ -36,9 +25,9 @@ const ChannelAddMessage = ({
           <Box height="50px" flexGrow={1}>
             <TextField
               sx={{
-                borderRadius: '50px',
-                padding: '0.5em',
-                width: '100%',
+                borderRadius: "50px",
+                padding: "0.5em",
+                width: "100%",
               }}
               onChange={(e) => setNewMessage(e.target.value)}
               value={newMessage}
@@ -49,10 +38,10 @@ const ChannelAddMessage = ({
               className={classes.profile_section__icon_more}
               onClick={() => {
                 handleAddNewMessage(newMessage);
-                setNewMessage('');
+                setNewMessage("");
               }}
               sx={{
-                paddingRight: '1.5em',
+                paddingRight: "1.5em",
               }}
               variant="text"
               disabled={!newMessage}
